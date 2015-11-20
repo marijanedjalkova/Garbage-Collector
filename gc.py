0ca8043574fa9fde54f802f03afc5f58c4b476b6
marijanedjalkova <nedyalkova.marya@gmail.com>
2015-11-20 01:02:46 +0000

    copying between two spaces fine

--- gc.py
+++ gc.py
@@ -3,7 +3,6 @@ class GarbageCollector:
 		self.heap = []
 		self.GENERATION_SIZE = 100
 		self.current_moving_index = 0 # this marks where to put the next thing
-		self.current_divide_index = 0 # this marks where the heap ended before the current garbage collection
 		self.current_tracing_index = 0 # this shows where we are in the old section
 		self.INT = 11
 		self.STRING = 12
@@ -17,58 +16,11 @@ class GarbageCollector:
 		self.TRUE = 1
 		self.FALSE = 0
 		self.NIL = -1
+		self.moved_roots = [] # this is for preserving roots when doing gc > once
 
+	"""This is legacy, not needed"""
 	def isTag(self, item):
 		return item in ["INT", "STRING", "BOOL", "CONS", "VECTOR", "ARRAY", "EXCEPTION", "IND", "VAR", "FWD"]
-
-	""" This is old version. Don't use it"""
-	def move_pointer_block(self):
-		
-		new_block_start = self.current_moving_index
-		# print "WILL PASTE HERE: " + str(current_moving_index)
-		saved_old_locations = []
-		### start of do-while imitation: copying the block 
-		# print "copying " + str(heap[current_tracing_index])
-		self.heap[self.current_moving_index] = self.heap[self.current_tracing_index]
-		saved_old_locations.append(self.current_tracing_index)
-		self.current_tracing_index += 1
-		self.current_moving_index += 1
-
-		while (not self.isTag(self.heap[self.current_tracing_index])):
-			# copy it to the new place
-
-			# print "copying " + str(heap[current_tracing_index])
-			self.heap[self.current_moving_index] = self.heap[self.current_tracing_index]
-			saved_old_locations.append(self.current_tracing_index)
-			self.current_tracing_index += 1
-			self.current_moving_index += 1
-		### end of do-while imitation
-		
-		# at this point the block is copied to a new place, need to replace it with pointers
-		self.heap[saved_old_locations[0]] = "FWD"
-		self.heap[saved_old_locations[1]] = new_block_start
-		if len(saved_old_locations)>2:
-			for item in saved_old_locations[2:]:
-				self.heap[item] = None
-		#now the new block is in place and the old place points to it
-
-	""" This is old version. Don't use it"""
-	def copy_block(self, from_index):
-		saved_old_locations = []
-		# print "copying " + str(heap[from_index])
-		self.heap[self.current_moving_index] = self.heap[from_index]
-		saved_old_locations.append(from_index)
-		from_index += 1
-		self.current_moving_index += 1
-
-		while (not self.isTag(self.heap[from_index])):
-			# copy it to the new place
-			# print "copying " + str(heap[from_index])
-			self.heap[self.current_moving_index] = self.heap[from_index]
-			saved_old_locations.append(from_index)
-			self.current_moving_index += 1
-			from_index += 1
-		return saved_old_locations
 
 	def print_status(self, desc):
 		print "--------------"
@@ -79,77 +31,10 @@ class GarbageCollector:
 		print "tracing " + str(self.current_tracing_index)
 		print "________"
 
-	""" This is old version. Don't use it"""
-	def process_pointer2(self, begin_index, end_index):
-		if self.heap[begin_index] == "IND":
-			begin_index += 1
-			return False
-
-		copied_pointer = self.heap[begin_index] # copied pointer is 6
-		# now have to copy everything for that pointer
-		new_block_start = self.current_moving_index
-		saved_old_locations = self.copy_block(copied_pointer) # copied INT 2 into the new place
-		# old locs is 6 and 7
-		# new block start is where we started putting INT from int 2
-		# now have to put fwd new_block_start into old locs
-		# also have to put IND new_block_start instead of heap[begin_index]
-
-
-		# self.print_status("AFTER COPYING ORIGINAL VALUES")
-			
-		# putting fwd new_block into old locs
-		self.heap[saved_old_locations[0]] = "FWD"
-		self.heap[saved_old_locations[1]] = new_block_start
-		if len(saved_old_locations)>2:
-			for item in saved_old_locations[2:]:
-				self.heap[item] = None
-
-		# putting reference to new_block_start into heap[begin_index]
-		self.heap[begin_index] = new_block_start
-		return True
-
-	""" This is old version. Don't use it"""
-	def process_ind2(self):
-		
-		# take all the pointers till the next tag
-		# begin index marks the beginning of new block in the new space
-		begin_index = self.current_moving_index
-		self.move_pointer_block()
-		end_index = self.current_moving_index
-		# self.print_status("AFTER MOVING POINTER BLOCK")
-		# now I have to copy all the original values for every pointer I just copied
-		while (begin_index < end_index):
-			# copy that value to the new place
-			result = self.process_pointer(begin_index, end_index)
-			if result == True:
-
-				begin_index += 1
-				self.current_moving_index += 1
-				# self.print_status("AFTER FINISHING WORK WITH A POINTER")
-			else:
-				begin_index += 1
-
-	""" This is old version. Don't use it"""
-	def collect_garbage2(self):
-		while self.current_tracing_index < self.current_divide_index:
-			
-			cell = self.heap[self.current_tracing_index]
-			
-			if cell == "IND":
-				self.process_ind()
-				self.current_tracing_index -= 1
-				self.current_moving_index -= 1
-			# self.print_status("AFTER PROCESSING IND")
-			self.current_tracing_index += 1
-		# have to clean the previous space now
-		for i in range(0, self.current_divide_index):
-			self.heap[i] = None
-		# TODO update divide index?
-		# TODO promote to next generation?
-
 
 	def process_int(self, index):
 		# length of the block is 2
+		self.moved_roots.append(self.current_moving_index)
 		self.heap[self.current_moving_index] = self.heap[index]
 		self.heap[index] = "FWD"
 		self.heap[self.current_moving_index + 1] = self.heap[index + 1]
@@ -167,6 +52,7 @@ class GarbageCollector:
 			# length of the block is 2
 			string_tuple = self.mapping_table.pop(string_code) # returns ("myVar", False)
 			self.mapping_table[string_code] = (string_tuple[0], True) # mark as checked
+			self.moved_roots.append(self.current_moving_index)
 			self.heap[self.current_moving_index] = self.heap[index]
 			self.heap[index] = "FWD"
 			self.heap[self.current_moving_index + 1] = self.heap[index + 1]
@@ -174,6 +60,7 @@ class GarbageCollector:
 			self.current_moving_index += 2
 
 	def process_bool(self, index):
+		self.moved_roots.append(self.current_moving_index)
 		self.heap[self.current_moving_index] = self.heap[index]
 		self.heap[index] = "FWD"
 		self.heap[self.current_moving_index + 1] = self.heap[index + 1]
@@ -187,10 +74,10 @@ class GarbageCollector:
 		# from_pointer gives index of a heap cell where this object was referenced
 		# e.g. if after copying there was a reference to cell 6 in cell 35, 
 		# from index is 35
-		print "p is " + str(from_index)
-		print "heap at p is " + str(pointer_index)
+		
 		
 		tag = self.heap[pointer_index]
+		
 		
 		return self.process_tag(tag, pointer_index, from_index)
 
@@ -198,6 +85,7 @@ class GarbageCollector:
 	def process_block(self, block_size, overhead, index):
 		# this is for common blocks such as cons, arrays, vectors
 		# since they have a lot in common
+		self.moved_roots.append(self.current_moving_index)
 		for i in range(0, block_size):
 			self.heap[self.current_moving_index + i] = self.heap[index + i]
 			if i == 0:
@@ -257,7 +145,7 @@ class GarbageCollector:
 			exception_tuple = self.mapping_table.pop(exception_code) # returns ("myVar", False)
 			
 			self.mapping_table[exception_code] = (exception_tuple[0], True) # mark as checked
-			
+			self.moved_roots.append(self.current_moving_index)
 			self.heap[self.current_moving_index] = self.heap[index]
 			self.heap[index] = "FWD"
 			
@@ -298,6 +186,7 @@ class GarbageCollector:
 			# length of the block is 2
 			var_tuple = self.mapping_table.pop(var_code) # returns ("myVar", False)
 			self.mapping_table[var_code] = (var_tuple[0], True) # mark as checked
+			self.moved_roots.append(self.current_moving_index)
 			self.heap[self.current_moving_index] = self.heap[index]
 			self.heap[index] = "FWD"
 			self.heap[self.current_moving_index + 1] = self.heap[index + 1]
@@ -348,8 +237,19 @@ class GarbageCollector:
 			self.process_pointer(root, root)
 			
 		self.print_status("BEFORE CLEANUP")
-		for i in range(0, self.current_divide_index):
-			self.heap[i] = None	
+		for i in range(0, self.TO):
+			self.heap[i] = None
+		self.swap_spaces()
+		self.roots = self.moved_roots
+		self.moved_roots = []
+		self.clean_mapping_table()
+	
+
+	def swap_spaces(self):
+		tmp = self.TO
+		self.TO = self.FROM
+		self.FROM = tmp
+		self.current_moving_index = self.TO
 
 
 	def initialise_heap(self):
@@ -366,24 +266,23 @@ class GarbageCollector:
 		
 		self.heap.extend(["INT", 23])
 
-		self.heap.extend(["VECTOR", 3, 2, 6, 6])
+		#self.heap.extend(["VECTOR", 3, 2, 6, 6])
 
-		self.heap.extend(["ARRAY", 2, 3, 2, 0, 6, 4, 0, 6, 4])
+		#self.heap.extend(["ARRAY", 2, 3, 2, 0, 6, 4, 0, 6, 4])
 		
 		self.heap.extend(["CONS", 3, 7])
-		
-		self.current_tracing_index = 0
-		self.current_moving_index = len(self.heap)
-		self.current_divide_index = len(self.heap)
-		for i in range(0, 40):
+		for i in range(0, 50):
 			self.heap.append(None)
 		
-		# this index shows where we are in the old section
+		self.FROM = 0
+		self.TO = 25
+		self.current_moving_index = self.TO
+
 
 	def initialise_roots(self):
 		self.roots = []
 
-		self.roots.append(7)
+		self.roots.append(3)
 
 		
 		
@@ -414,8 +313,14 @@ def main():
 	# and also the current index shows the first empty cell after all the code
 	gc.print_status("INITIAL")
 	gc.collect_garbage()
-	gc.clean_mapping_table()
-	gc.print_status("FINAL")
+
+	gc.print_status("FINAL1")
+	gc.collect_garbage()
+	
+	gc.print_status("FINAL2")
+	gc.collect_garbage()
+	
+	gc.print_status("FINAL3")
 	print "finished execution successfully"
 
 
